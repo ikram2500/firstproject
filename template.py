35ab7559abf6c87b01d6cb6fb55b0f666625d484
Muhammad Ikram <62110177+ikram2500@users.noreply.github.com>
2023-12-29 10:37:41 -0500

    some changes

--- template.py
+++ template.py
@@ -2,23 +2,23 @@ import logging
 import os 
 from pathlib import Path
 list_of_files = [
-    
+
     ".github/workflows/.gitkeep",
     "src/__init__.py",
-    "srccomponents\__init__.py",
-    "src\components\data_ingestion.py",
-    "src\components\data_transformation.py",
-    "src\components\model_trainer.py",
-    "src\components\model_evaluation.py",
-    "src\pipeline\__init__.py",
-    "src\pipeline\ training_pipeline.py",
-    "src\pipeline\prediction_pipeline.py",
-    "src\utils\__init__.py",
-    "src\utils\utils.py",
-    "src\logger\logging.py",
-    "src\exception\exception.py"
-    "tests\unit\__init__.py",
-    "tests\integration\__init__.py",
+    "srccomponents/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/ training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py"
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
@@ -26,7 +26,7 @@ list_of_files = [
     "setup.cfg",
     "pyproject.toml",
     "tox.ini",
-    "experiment\experiments.ipynb"
+    "experiment/experiments.ipynb"
 ]
 
 for filepath in list_of_files:
